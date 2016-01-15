769ad227e576595759575069c4009cd22d8ce7be
Markus Dobmann <md@noqq.eu>
2016-01-15 08:51:03 +0100

    Remove unused methods from lib

--- routes/timeline.js
+++ routes/timeline.js
@@ -7,9 +7,6 @@ const debug = require("debug")("peerigon-tito-api:routes")
 const auth = require("../auth.json");
 
 const fetchTimeline = require("../lib").fetchTimeline;
-const fetchEventDetails = require("../lib").fetchEventDetails;
-const fetchEventReleases = require("../lib").fetchEventReleases;
-const fetchTimelineWithEventDetails = require("../lib").fetchTimelineWithEventDetails;
 
 router.get('/', (req, res, next) => {
     fetchTimeline(auth.api_token, "json")
