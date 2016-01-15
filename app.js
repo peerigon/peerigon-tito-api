8314cf49fc3ff377bee073289e238725ad860092
Markus Dobmann <md@noqq.eu>
2016-01-15 09:18:21 +0100

    Add Access-Control-Allow-Origin header

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const app = express();
 
 app.use(logger('dev'));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use('/timeline', timeline);
 
 // catch 404 and forward to error handler
